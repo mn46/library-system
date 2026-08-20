d49ef55592586b7e23c6a1f97088e181cedb73bf
marta <m.nowak033@gmail.com>
2026-08-20 17:13:09 +0000

    Add check for matching passwords in user controller

--- backend/requests/createUserRequest.js
+++ backend/requests/createUserRequest.js
@@ -1,8 +1,14 @@
 const { z } = require("zod");
 
-const createUserSchema = z.object({
-  email: z.email(),
-  password: z.string().min(10),
-});
+const createUserSchema = z
+  .object({
+    email: z.email(),
+    password: z.string().min(10),
+    repeatPassword: z.string(),
+  })
+  .refine((data) => data.password === data.repeatPassword, {
+    message: "Passwords do not match.",
+    path: ["repeatPassword"],
+  });
 
 module.exports = createUserSchema;
