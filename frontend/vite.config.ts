9eb618da0df95f069032b7aaf3cc470b61d7c6c7
marta <m.nowak033@gmail.com>
2026-09-19 07:44:43 +0000

    Install react router dev tools

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,10 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
+import { reactRouterDevTools } from "react-router-devtools";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter()],
+  plugins: [tailwindcss(), reactRouterDevTools(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
   },
